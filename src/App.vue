f4a20468ecae197c495f96496bd7e6c9fac5e4f0
断魂剑士 <1049300159@qq.com>
2018-12-06 21:01:02 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    	<HelloWorld :msg="echo('自动百度翻译，优先本地语言库')"/>
+    <br/>
+      原文:{{test}}
+    <br/>
+      翻译后:{{echo(test)}}
+      </br>
+      如果翻译的是变量，那么渲染内容会跟着变量的变化而变化，而且不影响变量原本的内容。
   </div>
 </template>
 
@@ -12,6 +18,14 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data(){
+    setTimeout(()=>{
+      this.test = '内容被变化了'
+    },2000);
+    return {
+      test:'我是一个简单的测试文字'
+    }
   }
 }
 </script>
