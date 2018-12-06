468dacf10d8990383832ed1facc65c331b84f46b
断魂剑士 <1049300159@qq.com>
2018-12-06 21:01:38 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import language from 'jj_language'
+
+language.initData({
+	from:'auto', 
+	to:getQueryString('to') || 'jp',
+	//list:{'例如我是中文咯':'data'},
+	//directory:'/language/',
+	automatic:true,
+});
+ 
+Vue.use(language);
 
 Vue.config.productionTip = false
 
